80eca7f54ef87c4277fa3513e86bb886dc7f6233
YashwanthMN1 <yashwnthmn1@gmail.com>
2025-09-04 20:35:58 +0530

    tested locally the setup

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,7 +1,28 @@
-# From project-root/
-# docker build -t fastapi-app1:latest ./app1
-# docker build -t fastapi-app2:latest ./app2
+# create a kind cluster
+kind create cluster --name test
+# =====================================================================
+# test the cluster
+kind get clusters
+kubectl cluster-info --context kind-test
+# =====================================================================
+# From kubernetes_local/
+# these commands are to build the docker images
+docker build -t fastapi-app1:1 ./app1
+docker build -t fastapi-app2:1 ./app2
+# =====================================================================
+# Load into kind with proper name
+kind load docker-image fastapi-app1:1 --name test
+kind load docker-image fastapi-app2:1 --name test
+# =====================================================================
+# apply the k8s manifests rom kubernetes_local/
+kubectl apply -f k8s/app1-deployment.yaml
+kubectl apply -f k8s/app2-deployment.yaml
+# =====================================================================
+# check the pods
+kubectl get pods
+# =====================================================================
+# port forward and test locally or with public ip address or through curl command
+kubectl port-forward svc/app1-service 8001:8001
+kubectl port-forward svc/app2-service 8002:8002
 
-# Load into kind
-kind load docker-image fastapi-app1:latest
-kind load docker-image fastapi-app2:latest
+
